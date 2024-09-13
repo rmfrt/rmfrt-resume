3a0ecd19590ccfe42604a2462d4ba345d8a1b64a
Rémi Forte <forte.remi@gmail.com>
2024-09-13 17:17:00 +0200

    Refactor CvCardDescription component to use ReactMarkdown for rendering

--- app/components/CvCardDescription.jsx
+++ app/components/CvCardDescription.jsx
@@ -1,8 +1,29 @@
-import { createMarkup } from "../utils/markdown";
+// import { createMarkup } from "../utils/markdown";
+import ReactMarkdown from "react-markdown";
 
 export default function CvCardDescription({ children }) {
     if (typeof children === "string" && children.trim() !== "") {
-        return <p className="mt-3 text-sm text-neutral-400" dangerouslySetInnerHTML={createMarkup(children)} />;
+        return (
+            <p className="mt-3 text-sm text-neutral-400">
+                <ReactMarkdown
+                    components={{
+                        p: ({ node, ...props }) => <>{props.children}</>,
+                        a: ({ href, children }) => (
+                            <a
+                                href={href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="underline hover:no-underline "
+                            >
+                                {children}
+                            </a>
+                        ),
+                    }}
+                >
+                    {children}
+                </ReactMarkdown>
+            </p>
+        );
     }
 
     return null;
